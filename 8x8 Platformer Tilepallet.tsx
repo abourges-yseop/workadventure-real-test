6e2a332effed74ef1f61f95d3043b6c94f4f4e8f
abourges-yseop <abourges@yseop.com>
2021-03-21 21:35:35 +0100

    feat: olympe map

---
+++ 8x8 Platformer Tilepallet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="8x8 Platformer Tilepallet" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../Téléchargements/evercrazy_8x8_tile_palette/8x8 Platformer Tilepallet.png" width="120" height="112"/>
+</tileset>
