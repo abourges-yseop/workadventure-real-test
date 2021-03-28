5666a397e5c643a17b1d9c84ac228172a8fe1810
abourges-yseop <abourges@yseop.com>
2021-03-28 17:47:21 +0200

    test

--- uroesch-Cute_Cloud_with_Rainbow_2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="uroesch-Cute_Cloud_with_Rainbow" tilewidth="32" tileheight="32" tilecount="700" columns="28">
- <image source="uroesch-Cute_Cloud_with_Rainbow.png" trans="000000" width="922" height="806"/>
-</tileset>
