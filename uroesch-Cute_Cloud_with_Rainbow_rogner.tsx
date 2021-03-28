106887215222bcb216dddd7293e47a85b4f70e55
abourges-yseop <abourges@yseop.com>
2021-03-28 17:25:24 +0200

    test

--- uroesch-Cute_Cloud_with_Rainbow_rogner.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="uroesch-Cute_Cloud_with_Rainbow_rogner" tilewidth="32" tileheight="32" tilecount="408" columns="24">
- <image source="../../Téléchargements/uroesch-CuteCloud/uroesch-Cute_Cloud_with_Rainbow_rogner.png" width="771" height="574"/>
-</tileset>
--- uroesch-Cute_Rain_Cloud_with_Rainbow.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="uroesch-Cute_Rain_Cloud_with_Rainbow" tilewidth="32" tileheight="32" tilecount="952" columns="28">
- <image source="../../Téléchargements/uroesch-CuteCloud/uroesch-Cute_Rain_Cloud_with_Rainbow.svg" width="921" height="1111"/>
-</tileset>
